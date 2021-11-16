0ef1ae065e94e0760080064818f54aeeafa7d204
Aleksander Kruk <kruk.aleksander@hotmail.com>
2021-11-16 11:05:11 +0100

    create css files, get video for banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import movie from "./media/pexels-bhumik-photography-6522668(2).mp4";
 const App = () => {
   return (
     <div className="App">
       <h1>ORTK project</h1>
+      <video src={movie}></video>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "normalize.css";
+import "./css/main.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
