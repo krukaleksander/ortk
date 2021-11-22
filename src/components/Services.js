f7b7ce9530fac2cc32fd92df3bd209441d03a90b
Aleksander Kruk <kruk.aleksander@hotmail.com>
2021-11-22 08:32:55 +0100

    change bg ok, need to fix change color

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,42 @@
 import { ImPowerCord } from "react-icons/im";
 import { GiFireZone } from "react-icons/gi";
+import PowerImage from "../media/bg_before_movie.jpeg";
+import GasImage from "../media/bg-gas.jpeg";
+import BasicBg from "../media/bg-services.jpeg";
+import BgSpan from "../media/gradient-power.jpeg";
 const Services = () => {
+  const changeBg = (medium = "power") => {
+    const container = document.querySelector(".services");
+
+    if (medium === "power") {
+      container.style.background = `url(${PowerImage})`;
+      container.style.backgroundSize = "cover";
+      container.style.backgroundRepeat = "no-repeat";
+    }
+    if (medium === "gas") {
+      container.style.background = `url(${GasImage})`;
+      container.style.backgroundSize = "cover";
+      container.style.backgroundRepeat = "no-repeat";
+    }
+  };
+
+  const returnBg = () => {
+    document.querySelector(".services").style.background = `url(${BasicBg})`;
+  };
   return (
     <div className="services">
-      <div className="services__circle services__circle--power">
+      <div
+        className="services__circle services__circle--power"
+        onMouseOver={() => changeBg()}
+        onMouseOut={() => returnBg()}
+      >
         <ImPowerCord /> <p>Prąd</p>
       </div>
-      <div className="services__circle services__circle--gas">
+      <div
+        className="services__circle services__circle--gas"
+        onMouseOver={() => changeBg("gas")}
+        onMouseOut={() => returnBg()}
+      >
         <GiFireZone /> <p>Gaz</p>
       </div>
     </div>
