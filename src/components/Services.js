67dac16ad152327d9704694a40162348b96c0c72
Aleksander Kruk <kruk.aleksander@hotmail.com>
2021-11-24 07:11:44 +0100

    services done without scale effect

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,28 +3,41 @@ import { GiFireZone } from "react-icons/gi";
 import PowerImage from "../media/bg_before_movie.jpeg";
 import GasImage from "../media/bg-gas.jpeg";
 import BasicBg from "../media/bg-services.jpeg";
-import BgSpan from "../media/gradient-power.jpeg";
+
 const Services = () => {
   const changeBg = (medium = "power") => {
     const container = document.querySelector(".services");
+    const powerColored = document.querySelector(".services__colored--energy");
+    const gasColored = document.querySelector(".services__colored--gas");
+    const plusColored = document.querySelector(".services__colored--plus");
 
     if (medium === "power") {
       container.style.background = `url(${PowerImage})`;
       container.style.backgroundSize = "cover";
       container.style.backgroundRepeat = "no-repeat";
+      powerColored.style.display = "block";
+      plusColored.style.display = "block";
     }
     if (medium === "gas") {
       container.style.background = `url(${GasImage})`;
       container.style.backgroundSize = "cover";
       container.style.backgroundRepeat = "no-repeat";
+      gasColored.style.display = "block";
+      plusColored.style.display = "block";
     }
   };
 
   const returnBg = () => {
     document.querySelector(".services").style.background = `url(${BasicBg})`;
+    document.querySelector(".services__colored--energy").style.display = "none";
+    document.querySelector(".services__colored--gas").style.display = "none";
+    document.querySelector(".services__colored--plus").style.display = "none";
   };
   return (
     <div className="services">
+      <div className="services__colored services__colored--energy"></div>
+      <div className="services__colored services__colored--gas"></div>
+      <div className="services__colored services__colored--plus"></div>
       <div
         className="services__circle services__circle--power"
         onMouseOver={() => changeBg()}
