f75b022c683fe8c8556c61ba7136a9d92c4af9dc
Aleksander Kruk <kruk.aleksander@hotmail.com>
2021-11-24 08:59:44 +0100

    services ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import { Video } from "./components/Video";
 import Services from "./components/Services";
+import Whyus from "./components/Whyus";
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Video />
       <Services />
+      <Whyus />
     </div>
   );
 };
---
+++ src/components/Whyus.js
@@ -0,0 +1,35 @@
+const Whyus = () => {
+  return (
+    <div className="why-us">
+      <h6>
+        <span></span>korzyści
+      </h6>
+      <h1>Dlaczego właśnie my?</h1>
+      <div className="why-us__container">
+        <div className="why-us__card">
+          <h2>Lorem impsum</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+            optio possimus qui consequatur, dolor quasi quisquam debitis.
+          </p>
+        </div>
+        <div className="why-us__card why-us__card--under">
+          <h2>Lorem impsum</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+            optio possimus qui consequatur, dolor quasi quisquam debitis.
+          </p>
+        </div>
+        <div className="why-us__card">
+          <h2>Lorem impsum</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+            optio possimus qui consequatur, dolor quasi quisquam debitis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Whyus;
